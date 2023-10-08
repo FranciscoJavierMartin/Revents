09626b869371a59e8755541bd501379949640cdc
Francisco <javiermartincuenca@gmail.com>
2023-10-08 23:02:55 +0200

    Event list

---
+++ src/features/events/dashboard/EventList.tsx
@@ -0,0 +1,12 @@
+import EventListItem from '@/features/events/dashboard/EventListItem';
+
+export default function EventList() {
+  return (
+    <>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </>
+  );
+}
